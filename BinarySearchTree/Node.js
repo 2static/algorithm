e4c84f38eed462927f07e6b38a1e0df96ad4ac80
妙峰 <bill.sz@autonavi.com>
2019-06-13 10:10:16 +0800

    init project

---
+++ BinarySearchTree/Node.js
@@ -0,0 +1,10 @@
+/**
+ * Created by songzheng on 2019/6/13.
+ */
+export class Node {
+    constructor(key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
