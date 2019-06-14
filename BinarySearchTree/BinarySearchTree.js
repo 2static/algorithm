5e0cbdf1a385536aa37b285efdcca7cce8ba4898
妙峰 <bill.sz@autonavi.com>
2019-06-14 10:06:02 +0800

    init project

---
+++ BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,44 @@
+/**
+ * Created by songzheng on 2019/6/14.
+ */
+import { Node } from './models/Node';
+
+export default class BinarySearchTree {
+    constructor(compareFn) {
+        this.compareFn = compareFn;
+        this.root = null;
+    }
+
+    insert(key) {
+
+    }
+
+    search(key) {
+
+    }
+
+    inOrderTraverse() {
+
+    }
+
+    preOrderTraverse() {
+
+    }
+
+    postOrderTraverse() {
+
+    }
+
+    min() {
+
+    }
+
+    max() {
+
+    }
+
+    remove(key) {
+
+    }
+}
+
